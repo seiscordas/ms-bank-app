4d45c6fa7a929ed82d3be3dc65995fc416ec710a
Kleber Martins <kleber@klsites.com.br>
2023-03-01 17:16:29 -0300

    Criado microsserviços card

---
+++ card/src/test/java/com/kl/card/CardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kl.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
