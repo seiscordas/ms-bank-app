a237911eedb74bbafbff1dc1f12eb46cf6336f3c
Kleber Martins <kleber@klsites.com.br>
2023-05-04 17:46:38 -0300

    add security oauth2 jwt

---
+++ gatewayserver/src/main/java/com/kl/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.kl.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity){
+        httpSecurity.authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/kl/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/kl/cards/**").authenticated()
+                        .pathMatchers("/kl/loans/**").permitAll())
+                .oauth2ResourceServer().jwt().jwtAuthenticationConverter(grantedAuthoritiesExtractor());
+        httpSecurity.csrf().disable();
+        return httpSecurity.build();
+    }
+
+    Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+}
